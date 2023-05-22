6b9f733aad6830a4ac240b22fadcdaf29fd8f26e
chat0933 <90390114+chat0933@users.noreply.github.com>
2023-05-22 12:29:42 +0200

    V0.1
    First version with some sudo code

---
+++ db.py
@@ -0,0 +1,3 @@
+#Create a local database to test 
+# Step two, move the database to a server 
+# Step three, make a connection from python to the server with the database
---
+++ ui.py
@@ -0,0 +1,8 @@
+# Make a UI that is easy to navigate
+
+
+# 1: First option would be to connect to the server or close the program
+# in the beginning make a version that does not connect to the internet
+# But in the end it needs to connect to the internet
+
+print("Wellcome to Nilla Games achivement scoreboard")
