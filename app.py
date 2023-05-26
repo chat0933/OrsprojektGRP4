2936a099f47eae1e9d62e4bc41215a745e8fc6bf
chat0933 <90390114+chat0933@users.noreply.github.com>
2023-05-26 22:09:45 +0200

    v1.4
    Major changes, beta version of app.py +websute works and small changes to ui.py. ThreadTest.py is created

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template
-import sqlite3 as sql
+import sqlite3
+import threading
 app=Flask(__name__)
+#MAKE A FUNCTION THAT RELOADS THE WEBPAGE, IT MIGHT BE JAVA SCRIPTS I NEED TO USE
 
-dbname=("Niila.db")
+con=("Niila.db")
 
-@app.route("/")
-def db():
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@app.route("/USERS")
+def USERS():
     print("EHJ")
-    return render_template("index.html")
+    connect = sqlite3.connect(con)
+    cur = connect.cursor()
+    cur.execute("SELECT * FROM USERS")
+
+    data = cur.fetchall()
+
+    return render_template("users.html", data=data)
 
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- ui.py
+++ ui.py
@@ -10,13 +10,14 @@
 
 # 3 Implement a function that makes changeing users possible
 
+# 4 Implement threaing in all classes 
+
 import Niiladb
 import sqlite3
+import threading
 
 dbname= ('Niila.db')    
 con = sqlite3.connect('Niila.db')
-
-
 
 def ui():
     emptyUser = ""
@@ -83,52 +84,13 @@ def ui():
 
                     elif menu == "3":
                         #note IF THE INPUT DOES NOT MATCH THE ID AND NAME OF ANY SAVED ROW IN THE DB THE PROGRAM WILL CRASH
-                        print("UPDATE YOU SCORE TEST")
+                        print("UPDATE YOUR SCORE TEST")
                         uid = input("Please enter your ID: ")
                         name = input("Please enter your name: ")
                         print("TEST, GIVE 10 POINTS ")
                         cur.execute("UPDATE USERS SET SCORE= SCORE+ 10 WHERE ID=(?) AND NAME =(?)",(uid, name))
-                        count= 2
-                        
-
-
-                    
-                    #NOT COMPLETED
-                    elif menu == "99":
-                        print("What would you like to update?\n")
-                        print("Press 1 if you fixed a bug")
-                        print("Press 2 if you have helped your teamate")
-                        print("Press any other button to go back to the main menu")
-                        choice = input()
-                        if choice == "1": 
-                            print('How manny bugs have you killed today?')
-                            bugs = input()
-                            if bugs =="1":
-                                print("Updating, you have fixd one bug today!!")
-                            elif bugs =="2":
-                                print("Updating, you have fixd two bugs today!!")
-                            elif bugs == "3":
-                                print("Updating, you have fixed three bugs today!!")
-                            elif bugs =="4":
-                                print("Updating, you have fixed four bugs today!!\n Well done")
-                            elif bugs =="5":
-                                print("Updating, you have fixed five bugs today!!!\n Well done, heres an achivement")
-                                #bugspray()
-                                #cur.execute("INSERT INTO u") note her skal der være to databaser som hver for deres data
-                        
-                        elif choice == "2":    
-                            #Vi kan altid lave denen del om til en mere passende arbejdsopgave
-                            print('What did you help your teamate completing?')
-                            print(" Pres 1 i helped my teamate with x")
-                            print(" Pres 2 i helped my teamate with y")
-                            print(" Pres 3 i helped my teamate with z")
-                            help = input()
-                            if help == "1":
-                                print("Hey good job, pat youself on the back")
-                            elif help == "2":
-                                print("hejsa")
-                            elif help == "3":
-                                print("Hejsa2 Godt klaret")
+                        print("WE NEED SOME MORE INPUTS THAT LETS US CHOSE THE ACHIVEMENT WE WANT TO ADD / ACTIVATE ")
+                        count= 2                                            
                     
                     elif menu == "admin":
                         print("Are you sure you would like to wipe the database of all its data?\n Press y if you want to delete all data, Press n if you don't want to wipe the database of all its data")
@@ -154,6 +116,39 @@ def ui():
                     
 ui()
 
-
-#def bugspray():
-#    print("You have unlocked the achivement 'Bug Sprayer!!'")
+# def NotUsedYet():
+#                     elif menu == "99":
+#                         print("What would you like to update?\n")
+#                         print("Press 1 if you fixed a bug")
+#                         print("Press 2 if you have helped your teamate")
+#                         print("Press any other button to go back to the main menu")
+#                         choice = input()
+#                         if choice == "1": 
+#                             print('How manny bugs have you killed today?')
+#                             bugs = input()
+#                             if bugs =="1":
+#                                 print("Updating, you have fixd one bug today!!")
+#                             elif bugs =="2":
+#                                 print("Updating, you have fixd two bugs today!!")
+#                             elif bugs == "3":
+#                                 print("Updating, you have fixed three bugs today!!")
+#                             elif bugs =="4":
+#                                 print("Updating, you have fixed four bugs today!!\n Well done")
+#                             elif bugs =="5":
+#                                 print("Updating, you have fixed five bugs today!!!\n Well done, heres an achivement")
+#                                 #bugspray()
+#                                 #cur.execute("INSERT INTO u") note her skal der være to databaser som hver for deres data
+                        
+#                         elif choice == "2":    
+#                             #Vi kan altid lave denen del om til en mere passende arbejdsopgave
+#                             print('What did you help your teamate completing?')
+#                             print(" Pres 1 i helped my teamate with x")
+#                             print(" Pres 2 i helped my teamate with y")
+#                             print(" Pres 3 i helped my teamate with z")
+#                             help = input()
+#                             if help == "1":
+#                                 print("Hey good job, pat youself on the back")
+#                             elif help == "2":
+#                                 print("hejsa")
+#                             elif help == "3":
+#                                 print("Hejsa2 Godt klaret")
